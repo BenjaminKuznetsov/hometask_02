184281bc2f9d0cf3c5c09c88695fef2b38842412
BenjaminKuznetsov <benjaminkuznetsoff@gmail.com>
2024-10-19 18:49:54 +0300

    1

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Request, Response } from "express"
 import { PATHS } from "../lib/paths"
-import {blogsRouter} from "./features/blogs-router";
+import { HttpStatusCodes } from "../lib/httpStatusCodes"
+import { blogsRouter } from "./features/blog/blogsRouter"
+import { db } from "./db"
 export const app = express()
-
-
 
 app.use(express.json())
 app.use(PATHS.BLOGS, blogsRouter)
 
 app.get(PATHS.HOME, (req: Request, res: Response) => {
-    let helloPhrase = "Hometask 02, V1"
-    res.send(helloPhrase)
+  let helloPhrase = "Hometask 02, V1"
+  res.send(helloPhrase)
 })
 
+app.delete(PATHS.CLEAR_DB, (req: Request, res: Response) => {
+  db.blogs = []
+  res.sendStatus(HttpStatusCodes.NoContent)
+})
